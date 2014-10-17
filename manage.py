ca028c264911d491718f27583039513d0cdcb3df
luckypupil <blakesadams@gmail.com>
2014-10-17 16:33:22 -0400

    Hello world

---
+++ manage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+
+from app import create_app
+from app import db
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+if __name__ == '__main__':
+    app.run()
