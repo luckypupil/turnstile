37c81eedd8690cfde924a3b419ce21ec556312b7
luckypupil <blakesadams@gmail.com>
2014-10-18 12:19:21 -0400

    Created hmtl scaffolding and basic routes

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from config import config
 
 from flask.ext.sqlalchemy import SQLAlchemy
-
+from flask.ext.bootstrap import Bootstrap
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 
 def create_app(config_version):
@@ -12,6 +13,8 @@ def create_app(config_version):
 	app.config.from_object(config[config_version])
 
 	db.init_app(app)
+
+	bootstrap.init_app(app)
 	 
 	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
--- app/main/routes.py
+++ app/main/routes.py
@@ -2,6 +2,23 @@ from flask import render_template
 from . import main
 
 @main.route('/')
-def hello_world():
-	return 'hello world!'
+def dashboard():
+	return render_template("main/dashboard.html",message='hello world!')
+
+@main.route('/list')
+def sku_list():
+	return render_template("main/sku_list.html",message='hello world!')
+
+@main.route('/stores')
+def sku_store():
+	return render_template("main/sku_store.html",message='hello world!')
+
+@main.route('/disposition')
+def dispo_list():
+	return render_template("main/dispo_list.html",message='hello world!')
+
+@main.route('/disposition/<product>')
+def dispo_profile(product):
+	return render_template("main/dispo_profile.html",message='hello world!')
+
 
