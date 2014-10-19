e00e15995a7b921f159f71ca15bc152f5dc8c6f3
luckypupil <blakesadams@gmail.com>
2014-10-19 13:06:11 -0400

    Added flask script and setup shell context

--- manage.py
+++ manage.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 import os
+from flask.ext.script import Manager
+
 
 from app import create_app
-from app import db
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+#app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+manager = Manager(create_app)
+
+@manager.shell
+def make_shell_context():
+    app = create_app()
+    from app import db, models
+    return dict(app=app, db=db, models=models)
+
+
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
