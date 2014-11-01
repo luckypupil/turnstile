880595ce9810791a37b64bfe8d9e9fb3455a305c
luckypupil <blakesadams@gmail.com>
2014-10-31 22:22:59 -0400

    Updated Ml for simple regression

--- launch.py
+++ launch.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python
 
 from coopr.opt import SolverFactory
-from optimize.test import make_model
+from optimize.opt_model import make_model
+from optimize.luckypupil import wk_demand
 
 
 def model_solver(solver="glpk"):
-    model = make_model()
+    data = wk_demand()
+    model = make_model(arg=data)
     instance = model.create()
     #instance.pprint()
     opt = SolverFactory(solver)
     results = opt.solve(instance)
-    print results.items()
+    print results
     instance.load(results)
-    print('*********break***********')
-    print (instance.x_2.value)
+    #print('*********break***********')
+    #print (instance.x_2.value)
 
 
 if __name__ == '__main__':
-    model_solver()    
+    print wk_demand()
+    model_solver()
--- optimize/opt_model.py
+++ optimize/opt_model.py
@@ -2,12 +2,13 @@
 from coopr.pyomo import *
 
 
-def make_model():
+def make_model(arg=6):
     model = ConcreteModel()
     model.x_1 = Var(within=NonNegativeReals)
     model.x_2 = Var(within=NonNegativeReals)
-    model.obj = Objective(expr=model.x_1 + 2*model.x_2)
-    model.con1 = Constraint(expr=3*model.x_1 + 4*model.x_2 >= 1)
+    model.k   = Param(initialize=arg)
+    model.obj = Objective(expr=arg*model.x_1 + arg*model.x_2)
+    model.con1 = Constraint(expr=model.k*model.x_1 + 4*model.x_2 >= 1)
     model.con2 = Constraint(expr=2*model.x_1 + 5*model.x_2 >= 2)
 
     return model
