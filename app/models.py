7cb38350308867a72d225b120da80087fcc709d5
luckypupil <blakesadams@gmail.com>
2014-11-10 15:02:41 -0500

    added fct to populate store_distro table

--- app/models.py
+++ app/models.py
@@ -9,48 +9,57 @@ import requests
 class User_Role(db.Model):
     __tablename__ = 'user_roles'
     id = db.Column(db.Integer, primary_key=True)
-    role = db.Column(db.String(32), unique=True)
+    name = db.Column(db.String(32), unique=True)
 
-    def __init__(self,role):
-        self.role = role
+    def __init__(self,name):
+        self.name = name
 
     def __repr__(self):
-        return self.role
+        return self.name
 
 
 class Segment(db.Model):
     __tablename__ = 'segments'
     id = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(32), unique=True)
+    name = db.Column(db.String(32), unique=True)
     
 
-    def __init__(self,type):
-        self.type = type
+    def __init__(self,name):
+        self.name = name
 
     def __repr__(self):
-        return self.type
+        return self.name
 
 
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(32))
-    last_name = db.Column(db.String(32))
+    first = db.Column(db.String(32))
+    last = db.Column(db.String(32))
     email = db.Column(db.String(64), unique=True) #nullable
     password = db.Column(db.String(64))#todo: add password hash#
+    phone = db.Column(db.String(10))
     company_id = db.Column(db.Integer, db.ForeignKey('companies.id'))
     role_id = db.Column(db.Integer, db.ForeignKey('user_roles.id'))
-    phone = db.Column(db.String(10))
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
     #toAdds:
     #category_permissions = db.Column(Manyto Many!)
     #category_admin = #
     #list_admin = db.Column(db.Boolean, default=False)
     #is_admin = db.Column(db.Boolean, default=False)
-    
 
     def __repr__(self):
         return self.email
+
+    def __init__(self,first,last,email,password,phone,company_id=None,role_id=None,store_id=None):
+        self.first = first
+        self.last = last
+        self.email = email
+        self.password = password
+        self.phone = phone
+        self.company_id = company_id
+        self.role_id = role_id
+        self.store_id = store_id
 
 categories = db.Table('cattable',
     db.Column('category_id', db.Integer, db.ForeignKey('categories.id')),
@@ -61,16 +70,16 @@ class Company(db.Model):
     __tablename__ = 'companies'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64),) #nullable
-    categories = db.relationship('Category',secondary=categories, 
-        backref=db.backref('companies', lazy='dynamic'))
+    street = db.Column(db.Text())
+    city = db.Column(db.String(32))
+    state = db.Column(db.String(2))
+    zipcd = db.Column(db.Integer)
     segment_id = db.Column(db.Integer, db.ForeignKey('segments.id'))
     stores = db.relationship('Store', lazy='dynamic')
     users = db.relationship('User', lazy='dynamic')
     skus = db.relationship('User', lazy='dynamic')
-    street = db.Column(db.Text())
-    city = db.Column(db.String(32))
-    state = db.Column(db.String(2))
-    zip = db.Column(db.Integer)
+    categories = db.relationship('Category',secondary=categories, 
+        backref=db.backref('companies', lazy='dynamic'))
     #toadd:
     #amazon_credentials =  db.relationships()
 
@@ -80,25 +89,27 @@ class Company(db.Model):
     def __repr__(self):
         return self.name
 
+    def __init__(self,name,street,city,state,zip,segment_id=None):
+        self.name = name
+        self.street =  street
+        self.city = city
+        self.state = state
+        self.zip = zipcd
+        self.segment_id = segment_id
+
 
 class Store(db.Model):
     __tablename__ = 'stores'
     id = db.Column(db.Integer, primary_key=True)
-    company_id = db.Column(db.Integer, db.ForeignKey('companies.id')) #nullable
     store_num = db.Column(db.Integer)#nullable, May need to change to name
     street = db.Column(db.Text())
     city = db.Column(db.String(32))
     state = db.Column(db.String(2))
-    zip = db.Column(db.Integer)
+    zipcd = db.Column(db.Integer)
     lat = db.Column(db.Float(6))
     lng = db.Column(db.Float(6))
     warehouse = db.Column(db.Boolean, default=False)
-    crrnt_gm_forecast = db.Column(db.Integer)
-    optml_gm_forecast = db.Column(db.Integer)
-    crrnt_sales_forecast = db.Column(db.Integer)
-    optml_sales_forecast = db.Column(db.Integer)
-    crrnt_sellthru_forecast = db.Column(db.Integer)
-    optml_sellthru_forecast = db.Column(db.Integer)
+    company_id = db.Column(db.Integer, db.ForeignKey('companies.id'))
     #todo: update coords to postgis
     #geo
     #ship_leads = db.relationships('')
@@ -110,53 +121,115 @@ class Store(db.Model):
     def get_metric(self,metric,crrnt_or_optml):
         pass
 
+    def __init__(self,store_num,street,city,state,zipcd,warehouse,lat=None,lng=None,company_id=None):
+        self.store_num = store_num
+        self.street = street
+        self.city = city
+        self.state = state
+        self.zipcd = zipcd
+        self.warehouse = warehouse
+        self.lat = lat
+        self.lng = lng
+
+
 class Store_Distribution(db.Model):
     __tablename__ = 'store_distribution'
     id = db.Column(db.Integer, primary_key=True)
     sku_id = db.Column(db.Integer, db.ForeignKey('skus.id')) #nullable
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id')) #nullable
-    ship_to_date = db.Column(db.Date)
+    receipt_dt = db.Column(db.Date)
     units = db.Column(db.Integer)
+    crrnt_price = db.Column(db.Integer)
+    optml_price= db.Column(db.Integer)
+    plan_gm_fc = db.Column(db.Integer)
+    crrnt_gm_fc = db.Column(db.Integer)
+    optml_gm_fc = db.Column(db.Integer)
+    plan_sales_fc = db.Column(db.Integer)
+    crrnt_sales_fc = db.Column(db.Integer)
+    optml_sales_fc = db.Column(db.Integer)
+    plan_sellthru_fc = db.Column(db.Integer)
+    crrnt_sellthru_fc = db.Column(db.Integer)
+    optml_sellthru_fc = db.Column(db.Integer)
+    #Liquidation Considerations#
+    lqdt_src = db.Column(db.Boolean, default=False)
+    
 
     def __repr__(self):
-        return 'Sku:{} - Store:{} - Units:{} - Shipped:{}'.format(self.sku,self.store_num,self.units,self.ship_to_date)
+        return 'SkuID:{} - StoreID:{} - Units:{} - Shipped:{} - CrrntPrice:{}'.format(self.sku_id,
+                                                                                self.store_id,self.units,self.receipt_dt,self.crrnt_price)
     
     def get_perf_list(self):
         pass
+
+    def __init__(self,receipt_dt, units,crrnt_price=None,optml_price=None,plan_gm_fc=None,crrnt_gm_fc=None,optml_gm_fc=None,
+                    plan_sales_fc=None,crrnt_sales_fc=None,optml_sales_fc=None,
+                    plan_sellthru_fc=None,crrnt_sellthru_fc=None,optml_sellthru_fc=None, lqdt_src=False):
+        self.receipt_dt = receipt_dt
+        self.units = units
+        self.crrnt_price = crrnt_price
+        self.optml_price = optml_price
+        self.plan_gm_fc = plan_gm_fc
+        self.crrnt_gm_fc = crrnt_gm_fc
+        self.optml_gm_fc = optml_gm_fc
+        self.plan_sales_fc = plan_sales_fc
+        self.crrnt_sales_fc = crrnt_sales_fc
+        self.optml_sales_fc = optml_sales_fc
+        self.plan_sellthru_fc = plan_sellthru_fc
+        self.crrnt_sellthru_fc = crrnt_sellthru_fc
+        self.optml_sellthru_fc = optml_sellthru_fc
+        self.lqdt_src = lqdt_src
 
 class SKU(db.Model):
     __tablename__ = 'skus'
     id = db.Column(db.Integer, primary_key=True)
     sku_num = db.Column(db.String(32), unique=True) #nullable
-    prod_name = db.Column(db.String(64)) #nullable
+    prod_nm = db.Column(db.String(64)) #nullable
     brand = db.Column(db.String(64))
-    cluster_id = db.Column(db.Integer, db.ForeignKey('clusters.id'))
-    subcategory_id = db.Column(db.Integer, db.ForeignKey('subcats.id'))
-    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
     org_price = db.Column(db.Integer) #nullable
-    current_price = db.Column(db.Integer) #nullable
     launch_dt = db.Column(db.Date)
     phase_out = db.Column(db.Date)
     unit_cost = db.Column(db.Integer)
-    opt_price= db.Column(db.Integer)
-    plan_gm_forecast = db.Column(db.Integer)
-    crrnt_gm_forecast = db.Column(db.Integer)
-    optml_gm_forecast = db.Column(db.Integer)
-    plan_sales_forecast = db.Column(db.Integer)
-    crrnt_sales_forecast = db.Column(db.Integer)
-    optml_sales_forecast = db.Column(db.Integer)
-    plan_sellthru_forecast = db.Column(db.Integer)
-    crrnt_sellthru_forecast = db.Column(db.Integer)
-    optml_sellthru_forecast = db.Column(db.Integer)
-    lqdt_recommended = db.Column(db.Boolean, default=False)
+    #Liquidation Considerations#
+    lqdt_watch = db.Column(db.Boolean, default=False)
+    lqdt_rec = db.Column(db.Boolean, default=False)
     on_market = db.Column(db.Boolean, default=False)
     lqdt_price = db.Column(db.Integer)
+    units_to_list = db.Column(db.Integer, default=0)
     lqdt_channel_id = db.Column(db.Integer, db.ForeignKey('channels.id'))
-    units_to_list = db.Column(db.Integer)
+    cluster_id = db.Column(db.Integer, db.ForeignKey('clusters.id'))
+    
     #shipping dimensions
     #promotions
     #activechannels
     #features
+    def __init__(self,sku_num,prod_nm,brand,org_price,launch_dt,phase_out,
+                    unit_cost,lqdt_watch,lqdt_rec,on_market,lqdt_price=None,units_to_list=None,
+                    lqdt_channel_id=None,cluster_id=None,category_id=None):
+
+        self.sku_num = sku_num
+        self.prod_nm = prod_nm
+        self.brand = brand
+        self.org_price = org_price
+        self.launch_dt = launch_dt
+        self.phase_out = phase_out
+        self.unit_cost = unit_cost
+        self.lqdt_watch = lqdt_watch
+        self.lqdt_rec = lqdt_rec
+        self.on_market = on_market
+        self.lqdt_price = lqdt_price
+        self.units_to_list = units_to_list
+        self.lqdt_channel_id = lqdt_channel_id
+        self.cluster_id = cluster_id
+        self.category_id = category_id
+
+
+    
+    def lqdt_store(self):
+        pass
+    
+    def avg_price(self):
+        pass
+
     def gm(self,price):
         return ((price - self.unit_cost)/self.unit_cost)
 
@@ -165,11 +238,11 @@ class SKU(db.Model):
         return (self.opt_price-self.current_price)
 	
     def gm_impact(self):
-        return (self.optml_gm_forecast-self.crrnt_gm_forecast)
+        return (self.optml_gm_fc-self.crrnt_gm_fc)
 
     #Data Pulled from Optimization model#
     def get_metric(self,metric='gm',crrnt_or_optml='crrnt'):
-        info = _skus['skus'][0]['xo22']['forecasts'][metric][crrnt_or_optml]
+        info = _skus['skus'][0]['xo22']['fcs'][metric][crrnt_or_optml]
         return info
 
 
@@ -181,27 +254,29 @@ class Cluster(db.Model):
     __tablename__ = 'clusters'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True) #nullable
-    skus = db.relationship('SKU', lazy='dynamic')
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
-
-
-class Subcategory(db.Model):
-    __tablename__ = 'subcats'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True) #nullable
     skus = db.relationship('SKU', lazy='dynamic')
+
+    def __init__(self,name,category_id=None):
+        self.name = name
+        self.category_id = category_id
 
 
 class Category(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True) #nullable
-    skus = db.relationship('SKU', lazy='dynamic')
     clusters = db.relationship('Cluster', lazy='dynamic')
+
+    def __init__(self,name):
+        self.name = name
 
 
 class Channel(db.Model):
     __tablename__ = 'channels'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True) #nullable
+
+    def __init__(self,name):
+        self.name = name
 #Other Skipped classes: pricing_cluster, features
--- dbsetup.py
+++ dbsetup.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python
 import psycopg2
+from app.models import SKU, Store, Store_Distribution
+from app import db
+from random import randint
 
 Models = {
 		'categories': ['name'],
-		'companies': ['name','street','city','state','zip'],
-		'segments': ['type'],
+		'companies': ['name','street','city','state','zipcd'],
+		'segments': ['name'],
 		'channels': ['name'],
-		'skus': ['sku_num','prod_name','brand','cluster_id','subcategory_id','category_id','org_price','current_price','launch_dt',
-			'phase_out','unit_cost','opt_price','plan_gm_forecast','crrnt_gm_forecast','optml_gm_forecast','plan_sales_forecast',
-			'crrnt_sales_forecast','optml_sales_forecast','plan_sellthru_forecast','crrnt_sellthru_forecast','optml_sellthru_forecast',
-			'lqdt_recommended','on_market','lqdt_price','units_to_list'],
-		'stores': ['store_num','street','city','state',	'zip','warehouse','crrnt_gm_forecast','optml_gm_forecast',
-			'crrnt_sales_forecast','optml_sales_forecast','crrnt_sellthru_forecast','optml_sellthru_forecast'],
-		'subcats': ['name'],
-		'clusters':['name', 'category_id'],
-		'user_roles':['role'],
-		'users':['first_name','last_name','email','password','phone']
+		'skus': ['sku_num','prod_nm','brand','org_price','launch_dt','phase_out',
+				'unit_cost','lqdt_watch','lqdt_rec','on_market','lqdt_price'],
+		'stores': ['store_num','street','city','state',	'zipcd','warehouse'],
+		'clusters':['name'],
+		'user_roles':['name'],
+		'users':['first','last','email','password','phone'],
+		'store_distribution':['sku_id','store_id','receipt_dt','units','crrnt_price','optml_price','plan_gm_fc','crrnt_gm_fc','optml_gm_fc','plan_sales_fc',
+			'crrnt_sales_fc','optml_sales_fc','plan_sellthru_fc','crrnt_sellthru_fc','optml_sellthru_fc','lqdt_src']
 		} 
 
 
@@ -38,5 +39,32 @@ def popdb(*tables):
     cur.close()
     conn.close()
 
+def store_distro():
+	skus = SKU.query.all()
+	stores = Store.query.all()
+	for sku in skus:
+		for store in stores:
+			units = randint(50,225)
+			crrnt_price = int(sku.org_price*randint(85,100)/100)
+			optml_price = int(crrnt_price*randint(90,100)/100)
+			plan_gm_fc = int(units*crrnt_price*.25)
+			crrnt_gm_fc = int(plan_gm_fc*randint(90,105)/100)
+			optml_gm_fc = int(crrnt_gm_fc*randint(100,115)/100)
+			plan_sales_fc = int(units*crrnt_price*.95)
+			crrnt_sales_fc = int(plan_sales_fc*randint(90,105)/100)
+			optml_sales_fc = int(crrnt_sales_fc*randint(100,115)/100)
+			plan_sellthru_fc = randint(80,100)
+			crrnt_sellthru_fc = plan_sellthru_fc - randint(1,9)
+			optml_sellthru_fc = crrnt_sellthru_fc + randint(1,12)
+			sku_store = Store_Distribution(sku.launch_dt,units,crrnt_price,optml_price,
+				plan_gm_fc,crrnt_gm_fc,optml_gm_fc,plan_sales_fc,crrnt_sales_fc,optml_sales_fc,
+				plan_sellthru_fc,crrnt_sellthru_fc,optml_sellthru_fc)    
+			sku_store.sku_id, sku_store.store_id = sku.id, store.id
+			# print (sku_store)
+			db.session.add(sku_store)
+			db.session.commit()
+
+
 if __name__ == '__main__':
-	popdb('categories','companies','segments','channels','skus','stores','subcats','clusters','user_roles','users')
+	#popdb('categories','companies','segments','channels','skus','stores','clusters','user_roles','users')
+	store_distro()
