2057f2ffefb07d840c0770ae5a0b4fd89812634b
luckypupil <blakesadams@gmail.com>
2014-11-11 19:26:42 -0500

    updated styling on pygal graphs

--- app/main/helper.py
+++ app/main/helper.py
@@ -7,6 +7,7 @@ import numpy as np
 import pygal
 from pygal import Config as PygalConfig
 from pygal.style import LightColorizedStyle as lcstyle
+from pygal.style import LightenStyle
 import random
 from sqlalchemy import func
 
@@ -97,12 +98,8 @@ def get_clust_met(skus):
     
     return units
 
-class turnconfig(PygalConfig):
-    style = lcstyle
-    font_size = 20
 
-
-def make_stackedbar(title,plan,forecast,opp,height=270,width=500):
+def make_stackedbar(title,plan,forecast,opp,height=330,width=500):
     # plt.style.use('ggplot')
     plan /=100000
     round(plan,0)
@@ -111,8 +108,8 @@ def make_stackedbar(title,plan,forecast,opp,height=270,width=500):
     opp/=100000
     round(opp,0)
     assert title and plan and forecast and opp, "missing params"
-    bar_chart = pygal.StackedBar(style=pygal.style.LightColorizedStyle, 
-            width=500, height=height,label_font_size=12, legend_box_size=6, value_font_size=12)
+    bar_chart = pygal.StackedBar(style=pygal.style.RedBlueStyle, width=500, height=height,
+        label_font_size=12, legend_box_size=6, value_font_size=12)
     bar_chart.title = title
     bar_chart.x_labels = ['Plan','Forecast']
     bar_chart.add('Plan', [plan, None])
@@ -123,7 +120,7 @@ def make_stackedbar(title,plan,forecast,opp,height=270,width=500):
 def make_wklybar(title,plan,forecast,opp):
     # plt.style.use('ggplot')
     assert title and plan and forecast and opp, "missing params"
-    bar_chart = pygal.StackedBar(style=pygal.style.LightColorizedStyle, height=400)
+    bar_chart = pygal.StackedBar(style=pygal.style.RedBlueStyle, height=400)
     bar_chart.title = title
     bar_chart.x_labels = map(str,range(12))
     bar_chart.add('Actual', [10,12,8,14,12,11,0,0,0,0,0,0])
